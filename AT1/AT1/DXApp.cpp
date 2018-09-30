6960d20e2ec80502b4e7bd01249d9a30fb12b34b
Ellie Dunstan <ellie2.dunstan@live.uwe.ac.uk>
2018-09-30 19:35:13 +0100

    basic window set up

---
+++ AT1/AT1/DXApp.cpp
@@ -0,0 +1,126 @@
+#include "DXApp.h"
+
+namespace
+{
+	//used to forward messages to user defined procedure function
+	DXApp* g_pApp = nullptr;
+}
+
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (g_pApp)
+	{
+		return g_pApp->MsgProc(hwnd, msg, wParam, lParam);
+	}
+	else
+	{
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+}
+
+DXApp::DXApp(HINSTANCE hInstance)
+{
+	m_hAppInstance = hInstance;
+	m_hAppWnd = NULL;
+	m_ClientWidth = 800;
+	m_ClientHeight = 600;	//REMEMBER TO CHANGE THESE WHEN NOT WORKING IN 4K
+	m_AppTitle = "Procedural Building Generation - DirectX11";
+	m_WndStyle = WS_OVERLAPPEDWINDOW;
+	g_pApp = this;
+}
+
+DXApp::~DXApp()
+{
+}
+
+int DXApp::Run()
+{
+	MSG msg = { 0 };
+
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			Update(0.0f);
+			Render(0.0f);
+		}
+	}
+	return static_cast<int>(msg.wParam);
+}
+
+bool DXApp::Init()
+{
+	if (!InitWindow())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool DXApp::InitWindow()
+{
+	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(WNDCLASSEX));
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.hInstance = m_hAppInstance;
+	wcex.lpfnWndProc = MainWndProc;
+	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = "DXAPPWNDCLASS";
+	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex))
+	{
+		OutputDebugString("FAILED TO CREATE WINDOW CLASS \nDXApp.cpp InitWindow()");
+		return false;
+	}
+
+	RECT r = { 0,0, m_ClientHeight, m_ClientHeight };
+	AdjustWindowRect(&r, m_WndStyle, false);
+	UINT width = r.right - r.left;
+	UINT height = r.bottom - r.top;
+
+	UINT x = GetSystemMetrics(SM_CXSCREEN) / 2 - width / 2;
+	UINT y = GetSystemMetrics(SM_CYSCREEN) / 2 - height / 2;
+
+	m_hAppWnd = CreateWindow("DXAPPWNDCLASS", m_AppTitle.c_str(), m_WndStyle,
+		x, y, width, height, NULL, NULL, m_hAppInstance, NULL);
+
+	if (!m_hAppWnd)
+	{
+		OutputDebugString("FAILED TO CREATE WINDOW \nDXApp.cpp InitWindow()");
+		return false;
+	}
+
+	ShowWindow(m_hAppWnd, SW_SHOW);
+
+	return true;
+}
+
+LRESULT DXApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+
+
+	return LRESULT();
+}
